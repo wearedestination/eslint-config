6c6bf23b5cff1260f555e8488b60d9cf5bde8d77
andyexeter <palmer.andy@gmail.com>
2023-11-21 15:48:17 +0000

    Update prettier config

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 module.exports = {
     parserOptions: {
-        parser: '@typescript-eslint/parser',
-        sourceType: 'module'
+        parser: "@typescript-eslint/parser",
+        sourceType: "module"
     },
-    extends: ['eslint-config-airbnb-base', 'plugin:@typescript-eslint/recommended'],
+    extends: ["eslint-config-airbnb-base", "plugin:@typescript-eslint/recommended"],
     rules: {
-        'curly': 'error',
-        'no-undef': 'off',
-        'indent': 'off',
-        'quotes': 'off',
-        'max-len': 'off',
-        'function-paren-newline': 'off',
-        'implicit-arrow-linebreak': 'off',
-        'comma-dangle': 'off',
-        'object-curly-spacing': 'off',
-        'lines-between-class-members': 'off',
-        'no-use-before-define': 'off',
-        'no-new': 'off',
-        'object-curly-newline': 'off',
-        'no-console': 'off',
-        'no-plusplus': 'off',
-        'arrow-parens': 'off',
-        'no-shadow': 'off',
-        'prefer-destructuring': 'off',
-        'operator-linebreak': 'off',
-        'func-names': ['error', 'as-needed'],
-        'no-confusing-arrow': [
-            'error',
+        curly: "error",
+        "no-undef": "off",
+        indent: "off",
+        quotes: "off",
+        "max-len": "off",
+        "function-paren-newline": "off",
+        "implicit-arrow-linebreak": "off",
+        "comma-dangle": "off",
+        "object-curly-spacing": "off",
+        "lines-between-class-members": "off",
+        "no-use-before-define": "off",
+        "no-new": "off",
+        "object-curly-newline": "off",
+        "no-console": "off",
+        "no-plusplus": "off",
+        "arrow-parens": "off",
+        "no-shadow": "off",
+        "prefer-destructuring": "off",
+        "operator-linebreak": "off",
+        "func-names": ["error", "as-needed"],
+        "no-confusing-arrow": [
+            "error",
             {
                 onlyOneSimpleParam: true
             }
         ],
-        'no-param-reassign': [
-            'error',
+        "no-param-reassign": [
+            "error",
             {
                 props: false
             }
         ],
-        'import/prefer-default-export': 'off',
-        'import/no-unresolved': 'off',
-        'import/extensions': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off'
+        "import/prefer-default-export": "off",
+        "import/no-unresolved": "off",
+        "import/extensions": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
     }
 };
