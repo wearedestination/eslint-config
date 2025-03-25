0f0b83e26bebeaeb777dacbc470154cbf2b6f40d
andyexeter <palmer.andy@gmail.com>
2025-03-25 11:23:08 +0000

    Allow unused vars with _ prefix

--- index.js
+++ index.js
@@ -48,5 +48,17 @@ module.exports = {
                 "default": "array-simple",
             },
         ],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                "args": "all",
+                "argsIgnorePattern": "^_",
+                "caughtErrors": "all",
+                "caughtErrorsIgnorePattern": "^_",
+                "destructuredArrayIgnorePattern": "^_",
+                "varsIgnorePattern": "^_",
+                "ignoreRestSiblings": true,
+            },
+        ],
     },
 };
